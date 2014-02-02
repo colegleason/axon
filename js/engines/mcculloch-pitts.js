7a3b5299b131291ed268388621f830a0a2c10837
Cole Gleason <colewashere@gmail.com>
2014-02-02 00:59:05 -0600

    It works. MWAHAHA

--- js/engines/mcculloch-pitts.js
+++ js/engines/mcculloch-pitts.js
@@ -2,19 +2,31 @@ var _ = require('underscore');
 
 var Node = require('../models/node');
 
-Node.prototype.update = function() {
-    this.activation = _.chain(this.edges)
-        .filter(function(e) { return e.target == this;})
+Node.prototype.updateInput = function() {
+    var node = this;
+    node.data.net_input = _.chain(node.edges)
+        .filter(function(e) { return e.target == node;})
         .reduce(function(sum, e) {
             return sum + e.data.weight * e.source.data.activation;
         }, 0)
         .value();
 };
 
-Node.prototype.active = function() {
+Node.prototype.updateActivation = function() {
     var threshold = this.data.threshold || 0;
-    var activation = this.data.activation || 0;
-    return activation > threshold;
+    var input = this.data.net_input || 0;
+    this.data.activation = (input > threshold) ? 1 : 0;
+};
+
+Node.prototype.active = function() {
+    return this.data.activation;
+};
+
+var Activator = function() {
+    this.data = {activation: 1};
+    this.edges = [];
+    return this;
 };
 
 module.exports.Neuron = Node;
+module.exports.Activator = Activator;
--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
-var Node = require('./engines/mcculloch-pitts').Neuron;
-var _ = require('underscore');
+var MP =  require('./engines/mcculloch-pitts');
+var Node = MP.Neuron;
+var Activator = MP.Activator;
 
+var _ = require('underscore');
 var selectedNode = null;
 
 var tool = "cursor";
@@ -13,24 +15,11 @@ window.changeTool = function(newTool) {
         default:
         tool = "cursor";
     }
-    console.log(tool);
     cursor.attr("class", tool);
 };
 
 var width = 600,
     height = 900;
-
-var force = d3.layout.force()
-        .size([width, height])
-        .nodes([]) // initialize with a single node
-        .charge(0)
-        .gravity(0)
-        .linkDistance(function(d) {
-            var x = d.source.x - d.target.x;
-            var y = d.source.y - d.target.y;
-            return Math.sqrt(x*x + y*y);
-        })
-        .on("tick", tick);
 
 var svg = d3.select("#main").append("svg")
         .attr("viewBox", "0 0 " + width + " " + height )
@@ -38,19 +27,34 @@ var svg = d3.select("#main").append("svg")
         .on("mousemove", mousemove)
         .on("click", clickBackground);
 
-var nodes = force.nodes(),
-    links = force.links(),
+// build the arrow.
+svg.append("svg:defs").selectAll("marker")
+    .data(["end"])      // Different link/path types can be defined here
+    .enter().append("svg:marker")    // This section adds in the arrows
+    .attr("id", String)
+    .attr("viewBox", "0 -5 10 10")
+    .attr("refX", 15)
+    .attr("refY", -1.5)
+    .attr("markerWidth", 10)
+    .attr("markerHeight", 10)
+    .attr("orient", "auto")
+    .append("svg:path")
+    .attr("d", "M0,-5L10,0L0,5");
+
+var nodes = [],
+    links = [],
+    activators = [],
     gnode = svg.selectAll("g.gnode"),
     node = svg.selectAll("circle.node"),
     link = svg.selectAll(".link"),
-    label = svg.selectAll(".label");
+    label = svg.selectAll(".label"),
+    activator = svg.selectAll("pulsar");
 
 var cursor = svg.append("circle")
     .attr("r", 30)
     .attr("transform", "translate(-100,-100)")
     .attr("class", "cursor");
 
-restart();
 
 function mousemove() {
     cursor.attr("transform", "translate(" + d3.mouse(this) + ")");
@@ -58,46 +62,45 @@ function mousemove() {
 
 function clickBackground() {
     if (d3.event.defaultPrevented) return; // ignore drag
-    var point = d3.mouse(this),
-        node = new Node();
-
-    node.x = point[0], node.y = point[1];
-
-    nodes.push(node);
+    var point = d3.mouse(this);
+    if (tool == 'cursor') {
+        var newNode = new Node();
+        newNode.x = point[0], newNode.y = point[1];
+        nodes.push(newNode);
+    } else if (tool == 'activator') {
+        var newActivator = new Activator();
+        newActivator.x = point[0], newActivator.y = point[1];
+        activators.push(newActivator);
+    }
     restart();
 }
 
 function dragstart(d, i) {
-    force.stop(); // stops the force auto positioning before you start dragging
     cursor.attr('class', 'blank-cursor');
 }
 
 function dragmove(d, i) {
-    d.px += d3.event.dx;
-    d.py += d3.event.dy;
     d.x += d3.event.dx;
     d.y += d3.event.dy;
-    // if (d.x > width) { d.x = width; d.px = width; }
-    // if (d.x < 0) { d.x = 0; d.px = 0; }
-    // if (d.y > height) { d.y = height; d.py = height; }
-    // if (d.y < 0) { d.y = 0; d.py = 0; }
-    tick(); // this is the key to make it work together with updating both px,py,x,y on d !
+    tick();
 }
 
 function dragend(d, i) {
     cursor.attr('class', tool);
-    d.fixed = true; // of course set the node to fixed so the force doesn't include the node in its auto positioning stuff
-    tick();
-    force.resume();
 }
 
-function tick() {
-    //_.each(nodes, function(node) { node.update();});
-
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
+function tick(cb) {
+    link.attr("d", function(d) {
+        var dx = d.target.x - d.source.x,
+            dy = d.target.y - d.source.y,
+            dr = Math.sqrt(dx * dx + dy * dy)*3;
+        return "M" +
+            d.source.x + "," +
+            d.source.y + "A" +
+            dr + "," + dr + " 0 0,1 " +
+            d.target.x + "," +
+            d.target.y;
+    });
 
     node.attr("cx", function(d) { return d.x; })
         .attr("cy", function(d) { return d.y; })
@@ -105,22 +108,29 @@ function tick() {
 
     label.attr('x', function(d) { return d.x + 10; })
         .attr('y', function(d) { return d.y + 10; });
+
+    activator
+        .attr('x', function(d) { return d.x;})
+        .attr('y', function(d) { return d.y;});
+
+    if (cb) cb();
 }
 
 function clickNode(d) {
     if (d3.event.defaultPrevented) return; // ignore drag
-    d3.event.stopPropagation();
+    d3.event.stopPropagation(); // don't also click background
     if (tool == 'activator') {
-        d.data.activation = 1;
+        if (!d.data.activation) d.data.activation = 1;
+        restart();
     } else if (tool == 'cursor') {
         if (!selectedNode) {
-            selectedNode = d;
-            node.filter(function(elem) { return elem == d;}).style("fill", "red");
+            selectedNode = {d:d, elem:this};
+            d3.select(this).classed('selected', true);
         } else {
-            if (d != selectedNode) {
-                links.push(Node.addEdge(selectedNode, d, {weight: 30}));
+            if (d != selectedNode.d) {
+                links.push(Node.addEdge(selectedNode.d, d, {weight: 1}));
             }
-            node.filter(function(elem) { return elem == selectedNode;}).style("fill", null);
+            d3.select(selectedNode.elem).classed('selected', false);
             selectedNode = null;
             restart();
         }
@@ -144,20 +154,46 @@ function restart() {
         .call(node_drag)
         .on("click", clickNode);
 
-    node = svg.selectAll('.node');
-
     gnode.append("text")
         .style("fill", "black")
         .classed('label', true)
-        .text(function(d) { return d.data.activation || 0;});
+        .text(function(d, i) { return d.name || i; });
 
-    label = svg.selectAll('.label');
+    activator = activator
+        .data(activators)
+        .enter()
+        .append("rect")
+        .attr("width", 10)
+        .attr("height", 10)
+        .classed('pulsar', true)
+        .call(node_drag)
+        .on("click", clickNode);
 
     link = link.data(links);
-
     link.enter()
-        .insert("line", ".gnode")
+        .insert('svg:path', '.gnode')
+        .attr("marker-end", "url(#end)")
         .classed("link", true);
 
-    force.start();
+    node = svg.selectAll('.node');
+    label = svg.selectAll('.label');
+    activator = svg.selectAll('.pulsar');
 }
+
+window.updateNodes = function() {
+    setTimeout(function() {
+        _.each(nodes, function(n) {
+            n.updateInput();
+        });
+        _.each(nodes, function(n) {
+            n.updateActivation();
+        });
+        tick();
+        window.updateNodes();
+    }, 1000);
+}
+
+restart();
+(function ui() {
+    setTimeout(tick.bind(null, ui), 100);
+})();
